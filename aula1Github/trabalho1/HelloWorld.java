fcae7d8fefadcf0658b6c9348bac1811d4d78e44
cbmagnus <darlan.evaldt@gmail.com>
2014-03-09 19:14:00 -0300

    classe pessoa auxiliar
    criada classe pessoa

--- aula1Github/trabalho1/HelloWorld.java
+++ aula1Github/trabalho1/HelloWorld.java
@@ -1,15 +1,13 @@
 package trabalho1;
 
-import java.util.Scanner;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Qual seu nome?");
-		String nome = scan.nextLine();
 		
-		System.out.printf("Hello %s", nome);
+		System.out.println("Qual seu nome?");
+		Pessoa pessoa = new Pessoa();
+		
+		System.out.printf("Hello %s", pessoa.nome);
 
 	}
 
