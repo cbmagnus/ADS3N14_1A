623393aab8c602632ae0764d165794742f7967bd
cbmagnus <darlan.evaldt@gmail.com>
2014-03-09 19:03:34 -0300

    teste 1 commit
    alterado de hello world para hello you. (teste)

--- aula1Github/trabalho1/HelloWorld.java
+++ aula1Github/trabalho1/HelloWorld.java
@@ -1,9 +1,15 @@
 package trabalho1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Qual seu nome?");
+		String nome = scan.nextLine();
+		
+		System.out.printf("Hello %s", nome);
 
 	}
 
