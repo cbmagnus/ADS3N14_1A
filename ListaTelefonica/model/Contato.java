1d44a41ea87b9616b36a6a01fec19886cea89107
cbmagnus <darlan.evaldt@gmail.com>
2014-03-23 16:11:19 -0300

    inicio

---
+++ ListaTelefonica/model/Contato.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Contato {
+	private String nome;
+	private String telefone;
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setTelefone(String fone){
+		this.telefone = fone;
+	}
+	
+	public String getTelefone(){
+		return telefone;
+	}
+
+}
