edd728cf00425a2552cc825a44f1675550270ff4
cbmagnus <darlan.evaldt@gmail.com>
2014-03-23 22:44:39 -0300

    alterado controle

--- ListaTelefonica/controle/ListaMVC.java
+++ ListaTelefonica/controle/ListaMVC.java
@@ -1,12 +1,16 @@
 package controle;
 
-
 public class ListaMVC {
 
 	public static void main(String[] args) {
 		Controller controller = new Controller();
+		Menu menu = new Menu();
+		
 		controller.iniciaLista();
 		controller.imprimeLista();
+		menu.ajuda();
+		menu.menu();
+		
 	}
 
 }
