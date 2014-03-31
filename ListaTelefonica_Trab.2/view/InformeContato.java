7f55fc910cb0e883eb074a6007d32cacce3ff934
cbmagnus <darlan.evaldt@gmail.com>
2014-03-30 22:34:34 -0300

    melhor!

--- ListaTelefonica_Trab.2/view/InformeContato.java
+++ ListaTelefonica_Trab.2/view/InformeContato.java
@@ -7,21 +7,6 @@ public class InformeContato {
 	String nome;
 	String fone;
 	
-	/*public ArrayList<Pessoa> informeContato(ArrayList<Pessoa> agenda){
-		Pessoa pes = new Pessoa();
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Digite o nome: ");
-		pes.setNome(scan.nextLine());
-		
-		scan = new Scanner(System.in);
-		System.out.println("Digite o telefone: ");
-		pes.setTelefone(scan.nextLine());
-		
-		agenda.add(pes);
-		return agenda;
-	}*/
-	
 	public void informaPesquisa(){
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Digite o nome: ");
@@ -33,15 +18,4 @@ public class InformeContato {
 		return this.nome;
 	}
 	
-	/*public void setNome(String nome){
-		this.nome = nome;
-	}
-	
-	public String getTelefone(){
-		return this.fone;
-	}
-	
-	public void setTelefone(String fone){
-		this.fone = fone;
-	}*/
 }
