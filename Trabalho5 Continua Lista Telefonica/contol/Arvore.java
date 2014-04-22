3b23ff74468b08197fd3aa937b8e8b8adf378ceb
cbmagnus <darlan.evaldt@gmail.com>
2014-04-21 21:40:57 -0300

    Iniciando as classes
    Criando tudo do inicio! aproveitando para tentar melhorar o código da
    lista telefonica!

---
+++ Trabalho5 Continua Lista Telefonica/contol/Arvore.java
@@ -0,0 +1,16 @@
+package contol;
+
+import model.Nodo;
+
+public class Arvore {
+	
+	private Nodo raiz;
+
+	public Arvore(Nodo raiz) { //construtor
+		this.raiz = raiz;
+	}
+	
+	
+	
+
+}
---
+++ Trabalho5 Continua Lista Telefonica/model/Nodo.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Nodo {
+	
+	private Contato contato;
+	private Nodo filhoEsquerdo;
+	private Nodo filhoDireito;
+
+	public Nodo(Contato contato, Nodo filhoEsquerdo, Nodo filhoDireito) {
+		this.contato = contato;
+		this.filhoEsquerdo = filhoEsquerdo;
+		this.filhoDireito = filhoDireito;
+	}
+
+	
+	public Contato getContato() {
+		return contato;
+	}
+
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+
+	public Nodo getFilhoEsquerdo() {
+		return filhoEsquerdo;
+	}
+
+	public void setFilhoEsquerdo(Nodo filhoEsquerdo) {
+		this.filhoEsquerdo = filhoEsquerdo;
+	}
+
+	public Nodo getFilhoDireito() {
+		return filhoDireito;
+	}
+
+	public void setFilhoDireito(Nodo filhoDireito) {
+		this.filhoDireito = filhoDireito;
+	}
+	
+	
+	
+}
