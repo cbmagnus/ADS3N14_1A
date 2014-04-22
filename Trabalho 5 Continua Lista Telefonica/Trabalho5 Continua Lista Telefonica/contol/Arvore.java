930fbc7d0ce902d6d198f8ff6cca53b651b98ca7
cbmagnus <darlan.evaldt@gmail.com>
2014-04-21 22:24:13 -0300

    iniciando novo projeto
    tive de fazer em um novo projeto porque estava entrando em conflico com
    outros pacotes de outros trabalhos.

---
+++ Trabalho 5 Continua Lista Telefonica/Trabalho5 Continua Lista Telefonica/contol/Arvore.java
@@ -0,0 +1,24 @@
+package contol;
+
+import model.Contato;
+import model.Nodo;
+
+public class Arvore {
+	Contato cont;
+	private Nodo raiz;
+
+	public Arvore(Nodo raiz) { //construtor
+		this.raiz = raiz;
+	}
+	
+	public void insere(String nome, int fone){
+		Nodo novoNodo = new Nodo();
+		
+		novoNodo.setContato(new Contato(nome, fone)); //forma diferente de inserção
+		novoNodo.getContato().setNome(nome);
+		novoNodo.getContato().setNumero(fone);
+		
+	}
+	
+
+}
---
+++ Trabalho 5 Continua Lista Telefonica/Trabalho5 Continua Lista Telefonica/view/MostraNodo.java
@@ -0,0 +1,9 @@
+package view;
+
+public class MostraNodo {
+	
+	public void mostraNodo(String nome, int numero){
+		System.out.printf("%s 		- 	%d", nome, numero);
+	}
+
+}
